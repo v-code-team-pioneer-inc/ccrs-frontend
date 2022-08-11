9e47d6e35a538af3c8800848368a3883b6f6aed7
soheyHashimoto <heyso1209@gmail.com>
2022-08-11 23:57:39 +0900

    [MODIFY]cityIdを指定できるようにする

--- hooks/weather/getWeather.js
+++ hooks/weather/getWeather.js
@@ -2,11 +2,11 @@ import useSWR from "swr";
 import { fetcher } from "~/utils/fetcher";
 
 const WEATHER_API_URL =
-  "https://weather.tsukumijima.net/api/forecast/city/130010";
+  "https://weather.tsukumijima.net/api/forecast/city/";
 
-export const getWeather = () => {
+export const getWeather = (cityId) => {
   const { data, err } = useSWR(
-    WEATHER_API_URL,
+    WEATHER_API_URL + cityId,
     fetcher
   );
 
