af7fd17ec078cc76cc14144ae1e067963acebbbc
soheyHashimoto <heyso1209@gmail.com>
2022-08-11 23:58:45 +0900

    [ADD]selectboxより場所を指定して天気を取得

--- pages/useful/weather.js
+++ pages/useful/weather.js
@@ -1,18 +1,35 @@
+import { useState } from "react";
 import { getWeather } from "~/hooks/weather/getWeather";
+import { cities } from "~/services/weather/cities";
 
 export default function Weather() {
-  const { data, err } = getWeather();
+  const [city, setCity] = useState(cities[0]);
+
+  const { data, err } = getWeather(city.id);
   if (err) return <div>failed to load</div>;
   if (!data) return <div>now loading...</div>;
 
   return (
     <div>
-      <h1>{ data.title }</h1>
+      <select
+        value={city.id}
+        onChange={(e) =>
+          setCity(cities.find((c) => c.id === Number(e.target.value)))
+        }
+      >
+        {cities.map((city) => (
+          <option key={city.id} value={city.id}>
+            {city.name}
+          </option>
+        ))}
+      </select>
+
+      <h1>{data.title}</h1>
       {data.forecasts.map((forecast) => (
         <ul>
-          <li key={ forecast.date }>{ forecast.date }</li>
+          <li key={forecast.date}>{forecast.date}</li>
           <ul>
-            <li key={ forecast.telop }>{ forecast.telop }</li>
+            <li key={forecast.telop}>{forecast.telop}</li>
           </ul>
         </ul>
       ))}
