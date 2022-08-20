c1aef87860b8e541ecca4be545a56dada79d2749
soheyHashimoto <heyso1209@gmail.com>
2022-08-21 02:12:45 +0900

    [ADD]多田さん実装部分

--- pages/rooms/index.jsx
+++ pages/rooms/index.jsx
@@ -1,7 +1,12 @@
+import styles from "~/styles/Home.module.css";
+import { ClassroomStatusTable } from "~/components/rooms/ClassroomStatusTable";
+
 export default function Rooms() {
   return (
     <div>
-      <h1>Rooms</h1>
+      <h1 className={styles.title}>教室状況検索</h1>
+
+      <ClassroomStatusTable />
     </div>
   );
 }
