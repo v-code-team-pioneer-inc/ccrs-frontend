65ff7e6b681d526f42ee2c0904a7e03721b4f35e
soheyHashimoto <heyso1209@gmail.com>
2022-08-21 01:56:07 +0900

    [ADD]藤原さん実装部分

---
+++ components/top/TodayAvailableClassroom.jsx
@@ -0,0 +1,14 @@
+// TODO: 藤原さん実装お願いします
+//       divの中に実装をお願いします
+// 　　　 CSSはTodayAvailableClassroom.module.cssを使用してください
+
+import styles from "~/styles/top/TodayAvailableClassroom.module.css";
+
+export const TodayAvailableClassroom = () => {
+  return (
+    <div>
+      {/* TODO: タイトルと横並びのカードの実装 */}
+
+    </div>
+  );
+};
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,15 @@
 import styles from '../styles/Home.module.css'
 import { ToSearchButton } from '~/components/top/ToSearchButton'
+import { TodayAvailableClassroom } from "~/components/top/TodayAvailableClassroom";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>
-        中央大学空き教室確認
-      </h1>
+      <h1 className={styles.title}>中央大学空き教室確認</h1>
+
+      <TodayAvailableClassroom />
+
+      <hr />
 
       <ToSearchButton />
 
