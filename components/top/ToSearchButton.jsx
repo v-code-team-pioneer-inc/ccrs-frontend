b5f46cd94ea0236e58868f647322f2a64b1ca9da
soheyHashimoto <heyso1209@gmail.com>
2022-08-21 01:48:03 +0900

    [ADD]みおさん実装部分

---
+++ components/top/ToSearchButton.jsx
@@ -0,0 +1,14 @@
+// TODO: みおさん実装お願いします
+//       divの中に実装をお願いします
+// 　　　 CSSはToSearchButton.module.cssを使用してください
+
+import styles from "~/styles/top/ToSearchButton.module.css";
+
+export const ToSearchButton = () => {
+  return (
+    <div>
+      {/* TODO: ボタン二つ表示。デザインは変えてもらって大丈夫です */}
+
+    </div>
+  );
+};
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { ToSearchButton } from '~/components/top/ToSearchButton'
 
 export default function Home() {
   return (
     <div className={styles.container}>
 
+      <ToSearchButton />
 
       <footer className={styles.footer}>
         Powered by ぱいおにあ
