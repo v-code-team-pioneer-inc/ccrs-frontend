39c2516c05ffaf8454e615e312c9fd159fcf9d40
soheyHashimoto <heyso1209@gmail.com>
2022-08-21 01:51:48 +0900

    [ADD]タイトルのデザインを再現する

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,12 +4,13 @@ import { ToSearchButton } from '~/components/top/ToSearchButton'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <h1 className={styles.title}>
+        中央大学空き教室確認
+      </h1>
 
       <ToSearchButton />
 
-      <footer className={styles.footer}>
-        Powered by ぱいおにあ
-      </footer>
+      <footer className={styles.footer}>Powered by ぱいおにあ</footer>
     </div>
-  )
+  );
 }
