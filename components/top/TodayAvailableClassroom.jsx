073491408bac550cd7dc140e67ef3dd175cd1b0f
soheyHashimoto <heyso1209@gmail.com>
2022-08-23 01:47:24 +0900

    [ADD]stylesを適用

--- components/top/TodayAvailableClassroom.jsx
+++ components/top/TodayAvailableClassroom.jsx
@@ -9,26 +9,26 @@ export const TodayAvailableClassroom = () => {
   return (
     <div>
       {/* TODO: タイトルと横並びのカードの実装 */}
-      <ul>
-        <li>
-          <p className="class_room">F301</p>
-          <p className="time">14:00-15:30</p>
-          <p className="decision">行くかも</p>
+      <ul className={styles.ul}>
+        <li className={styles.li}>
+          <p className={styles.class_room}>F301</p>
+          <p className={styles.time}>14:00-15:30</p>
+          <p className={styles.decision}>行くかも</p>
         </li>
-        <li>
-          <p className="class_room">F305</p>
-          <p className="time">11:00-12:30</p>
-          <p className="decision">行くかも</p>
+        <li className={styles.li}>
+          <p className={styles.class_room}>F305</p>
+          <p className={styles.time}>11:00-12:30</p>
+          <p className={styles.decision}>行くかも</p>
         </li>
-        <li>
-          <p className="class_room">F306</p>
-          <p className="time">14:00-15:30</p>
-          <p className="decision">行くかも</p>
+        <li className={styles.li}>
+          <p className={styles.class_room}>F306</p>
+          <p className={styles.time}>14:00-15:30</p>
+          <p className={styles.decision}>行くかも</p>
         </li>
-        <li>
-          <p className="class_room">F306</p>
-          <p className="time">15:30-17:00</p>
-          <p className="decision">行くかも</p>
+        <li className={styles.li}>
+          <p className={styles.class_room}>F306</p>
+          <p className={styles.time}>15:30-17:00</p>
+          <p className={styles.decision}>行くかも</p>
         </li>
       </ul>
     </div>
