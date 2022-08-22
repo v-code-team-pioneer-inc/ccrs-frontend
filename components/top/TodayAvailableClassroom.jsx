9cdaa51d011e4fe2d08723cd27cac0800718da31
Ryu Fujiwara <anchianchobi@gmail.com>
2022-08-23 00:58:12 +0900

    Fujiwara-firstcommit

--- components/top/TodayAvailableClassroom.jsx
+++ components/top/TodayAvailableClassroom.jsx
@@ -9,7 +9,28 @@ export const TodayAvailableClassroom = () => {
   return (
     <div>
       {/* TODO: タイトルと横並びのカードの実装 */}
-
+      <ul>
+        <li>
+          <p className="class_room">F301</p>
+          <p className="time">14:00-15:30</p>
+          <p className="decision">行くかも</p>
+        </li>
+        <li>
+          <p className="class_room">F305</p>
+          <p className="time">11:00-12:30</p>
+          <p className="decision">行くかも</p>
+        </li>
+        <li>
+          <p className="class_room">F306</p>
+          <p className="time">14:00-15:30</p>
+          <p className="decision">行くかも</p>
+        </li>
+        <li>
+          <p className="class_room">F306</p>
+          <p className="time">15:30-17:00</p>
+          <p className="decision">行くかも</p>
+        </li>
+      </ul>
     </div>
   );
 };
