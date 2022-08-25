8530863654310e5a70f1eec9bd9b874b195b34ca
Ryu Fujiwara <anchianchobi@gmail.com>
2022-08-25 21:37:59 +0900

    見た目変更

--- components/top/TodayAvailableClassroom.jsx
+++ components/top/TodayAvailableClassroom.jsx
@@ -10,27 +10,28 @@ export const TodayAvailableClassroom = () => {
     <div>
       {/* TODO: タイトルと横並びのカードの実装 */}
       <ul className={styles.ul}>
-        <li className={styles.li}>
-          <p className={styles.class_room}>F301</p>
-          <p className={styles.time}>14:00-15:30</p>
-          <p className={styles.decision}>行くかも</p>
-        </li>
-        <li className={styles.li}>
-          <p className={styles.class_room}>F305</p>
-          <p className={styles.time}>11:00-12:30</p>
-          <p className={styles.decision}>行くかも</p>
-        </li>
-        <li className={styles.li}>
-          <p className={styles.class_room}>F306</p>
-          <p className={styles.time}>14:00-15:30</p>
-          <p className={styles.decision}>行くかも</p>
-        </li>
-        <li className={styles.li}>
-          <p className={styles.class_room}>F306</p>
-          <p className={styles.time}>15:30-17:00</p>
-          <p className={styles.decision}>行くかも</p>
-        </li>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </ul>
     </div>
+  );
+};
+
+export const Card = () => {
+  return (
+    <li className={styles.li}>
+      <p className={styles.class_room}>F301</p>
+      <p className={styles.time}>14:00-15:30</p>
+      <p><button type="button" className={styles.decision}>行くかも</button></p>
+    </li>
   );
 };
