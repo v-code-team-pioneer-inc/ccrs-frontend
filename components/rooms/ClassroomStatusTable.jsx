ad1de262e71717d3a7607989162cc9be8730367f
soheyHashimoto <heyso1209@gmail.com>
2022-08-29 11:22:15 +0900

    [MODIFY]エラーの修正

--- components/rooms/ClassroomStatusTable.jsx
+++ components/rooms/ClassroomStatusTable.jsx
@@ -8,35 +8,32 @@ import styles from "~/styles/rooms/ClassroomStatusTable.module.css";
 export const ClassroomStatusTable = () => {
   return (
     <div>
-      <body>
-        <room_table border="2" style="border-collapse: collapse">
-            <tr>
-              <th>時間/教室</th> <th>F301</th> <th>F302</th> <th>F303</th> <th>F304</th> <th>F305</th>
-            </tr>          
-            <tr>
-              <th id="main">9:00-10:40</th> <td></td> <td></td> <td></td> <td></td> <td></td>
-            </tr>        
-            <tr>
-              <th id="main">10:50-12:30</th> <td></td> <td></td> <td></td> <td></td> <td></td>
-            </tr>
-            <tr>
-              <th id="main" style="background-color:darkgray ;">昼休み</th> <td></td> <td></td> <td></td> <td></td> <td></td>
-            </tr>
-            <tr>
-              <th id="main">13:20-15:00</th> <td></td> <td></td> <td></td> <td></td> <td></td>
-            </tr>
-            <tr>
-              <th id="main">15:10-16:50</th> <td></td> <td></td> <td></td> <td></td> <td></td>
-            </tr>
-            <tr>
-              <th id="main">17:00-18:40</th> <td></td> <td></td> <td></td> <td></td> <td></td>
-            </tr>
-            <tr>
-              <th id="main">18:50-20:30</th> <td></td> <td></td> <td></td> <td></td> <td></td>
-            </tr>
-          </room_table>
-</body>
-
+      <div>
+          <tr>
+            <th>時間/教室</th> <th>F301</th> <th>F302</th> <th>F303</th> <th>F304</th> <th>F305</th>
+          </tr>
+          <tr>
+            <th >9:00-10:40</th> <td></td> <td></td> <td></td> <td></td> <td></td>
+          </tr>
+          <tr>
+            <th>10:50-12:30</th> <td></td> <td></td> <td></td> <td></td> <td></td>
+          </tr>
+          <tr>
+            <th>昼休み</th> <td></td> <td></td> <td></td> <td></td> <td></td>
+          </tr>
+          <tr>
+            <th >13:20-15:00</th> <td></td> <td></td> <td></td> <td></td> <td></td>
+          </tr>
+          <tr>
+            <th >15:10-16:50</th> <td></td> <td></td> <td></td> <td></td> <td></td>
+          </tr>
+          <tr>
+            <th >17:00-18:40</th> <td></td> <td></td> <td></td> <td></td> <td></td>
+          </tr>
+          <tr>
+            <th >18:50-20:30</th> <td></td> <td></td> <td></td> <td></td> <td></td>
+          </tr>
+      </div>
     </div>
   );
 };
