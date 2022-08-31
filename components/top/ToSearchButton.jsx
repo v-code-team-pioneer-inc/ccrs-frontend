99216eaaae74c63d102ade1e01e5e0e6638bb0f0
mioykim <a22.a8wm@g.chuo-u.ac.jp>
2022-08-31 18:28:24 +0900

    仮完成

--- components/top/ToSearchButton.jsx
+++ components/top/ToSearchButton.jsx
@@ -7,10 +7,13 @@ import styles from "~/styles/top/ToSearchButton.module.css";
 
 export const ToSearchButton = () => {
   return (
+  
     <div className={styles.container}>
+     
       <button type="button" className={styles.link_button}>
         空き教室を確認する
       </button>
+      
       <br/>
       <button type="button" className={styles.link_button2}>
         各教室の<br/>今後のスケジュールを<br/>確認する
