aa322af21c96680389c22f99fa098e8dd76feb37
kantarodaro <kantaro4free@gmail.com>
2022-09-01 18:52:04 +0900

    room_table.枠線変更

--- components/rooms/ClassroomStatusTable.jsx
+++ components/rooms/ClassroomStatusTable.jsx
@@ -7,33 +7,34 @@ import styles from "~/styles/rooms/ClassroomStatusTable.module.css";
 
 export const ClassroomStatusTable = () => {
   return (
-    <table className={styles.room_table}>
+    <table border="1" className={styles.room_table}>
       <thead>
         <tr>
-          <th>時間/教室</th> <th>F301</th> <th>F302</th> <th>F303</th> <th>F304</th> <th>F305</th>
+          <th >時間/教室</th> <th>F301</th> <th>F302</th> <th>F303</th> <th>F304</th> <th>F305</th> <th>F401</th>
         </tr>
       </thead>
       <tbody>
         <tr className={styles.room_tr}>
-          <th className={styles.room_main}>9:00-10:40</th> <td></td> <td></td> <td></td> <td></td> <td></td>
+          <th className={styles.room_main}>9:00-10:40</th> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td>
         </tr>
         <tr className={styles.room_tr}>
-          <th className={styles.room_main}>10:50-12:30</th> <td></td> <td></td> <td></td> <td></td> <td></td>
+          <th className={styles.room_main}>10:50-12:30</th> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td>
         </tr>
         <tr className={styles.room_tr}>
-          <th className={styles.room_main}>昼休み</th> <td></td> <td></td> <td></td> <td></td> <td></td>
+          <th className={styles.room_sub}>昼休み</th> <td className={styles.room_sub}></td> <td className={styles.room_sub}></td> 
+          <td className={styles.room_sub}></td> <td className={styles.room_sub}></td> <td className={styles.room_sub}></td> <td className={styles.room_sub}></td>
         </tr>
         <tr className={styles.room_tr}>
-          <th className={styles.room_main}>13:20-15:00</th> <td></td> <td></td> <td></td> <td></td> <td></td>
+          <th className={styles.room_main}>13:20-15:00</th> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td>
         </tr>
         <tr className={styles.room_tr}>
-          <th className={styles.room_main}>15:10-16:50</th> <td></td> <td></td> <td></td> <td></td> <td></td>
+          <th className={styles.room_main}>15:10-16:50</th> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td>
         </tr>
         <tr className={styles.room_tr}>
-          <th className={styles.room_main}>17:00-18:40</th> <td></td> <td></td> <td></td> <td></td> <td></td>
+          <th className={styles.room_main}>17:00-18:40</th> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td>
         </tr>
         <tr className={styles.room_tr}>
-          <th className={styles.room_main}>18:50-20:30</th> <td></td> <td></td> <td></td> <td></td> <td></td>
+          <th className={styles.room_main}>18:50-20:30</th> <td></td> <td></td> <td></td> <td></td> <td></td> <td></td>
         </tr>
         </tbody>
     </table>
