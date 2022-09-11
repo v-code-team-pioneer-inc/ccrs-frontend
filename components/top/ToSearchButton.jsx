5afe201903b16444b89ac241341d58364ee85f76
mioykim <a22.a8wm@g.chuo-u.ac.jp>
2022-09-11 17:13:22 +0900

    roomsへの遷移

--- components/top/ToSearchButton.jsx
+++ components/top/ToSearchButton.jsx
@@ -4,21 +4,22 @@
 //       http://localhost:3000/ に実装が反映されます
 
 import styles from "~/styles/top/ToSearchButton.module.css";
+import Link from "next/link"
 
 export const ToSearchButton = () => {
   return (
   
     <div className={styles.container}>
-     
+     <Link href="/rooms">
       <button type="button" className={styles.link_button}>
         空き教室を確認する
       </button>
-      
+     </Link>
       <br/>
       <button type="button" className={styles.link_button2}>
         各教室の<br/>今後のスケジュールを<br/>確認する
       </button>
-
+     
     </div>
   );
 };
